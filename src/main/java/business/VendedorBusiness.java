f2b78cbd07746e4ac88c8de7a97b8a76a5534dc7
Renan Roseno <rosenorenan@gmail.com>
2021-03-30 12:12:07 -0300

    CRUD de Vendedores finalizado

--- src/main/java/business/VendedorBusiness.java
+++ src/main/java/business/VendedorBusiness.java
@@ -1,31 +1,161 @@
 package business;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Scanner;
+
+import DAO.VendedorDAO;
+import entities.Cliente;
 import entities.Vendedor;
 
 public class VendedorBusiness implements Business<Vendedor>{
+	
+	public static VendedorDAO vendedorDAO = new VendedorDAO();
+	public static Scanner scan  = new Scanner(System.in);
 
 	@Override
 	public void salvar() {
-		// TODO Auto-generated method stub
+		System.out.println("/---INSERIR VENDEDOR---/");
+		Vendedor vendedor = new Vendedor();
 		
+		try {
+			scan.nextLine();
+			System.out.print("Nome:");
+			vendedor.setNome(scan.nextLine());
+			System.out.print("CPF:");
+			vendedor.setCpf(scan.nextLine());
+			System.out.print("Telefone:");
+			vendedor.setTelefone(scan.nextLine());
+			System.out.print("Data de nascimento:");
+			String dataNascimento = scan.nextLine().trim();
+			vendedor.setDataNascimento(this.convertData(dataNascimento));
+			
+			vendedorDAO.save(vendedor);
+		} catch(Exception e) {
+			throw new RuntimeException("Não foi possivel salvar um vendedor." + e);
+		}		
 	}
 
 	@Override
 	public void editar() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("/--- EDITAR VENDEDOR---/");
+		Vendedor vendedor = new Vendedor();
+
+		scan.nextLine();
+		System.out.print("Insira o código do Vendedor para editar:");
+		String codigo = scan.nextLine();
+
+		vendedor = vendedorDAO.findById(Long.parseLong(codigo));
+		if(vendedor != null) {
+			System.out.println("Vendedor:");
+			System.out.print(vendedor.toString());
+			System.out.println("\nQual campo deseja editar?");
+			System.out.println("1- Nome");
+			System.out.println("2- CPF");
+			System.out.println("3- Data de Nascimento");
+			System.out.println("4- Número de telefone");
+			System.out.println("5- Todos");
+
+			int op = Integer.parseInt(scan.nextLine());
+			switch(op) {
+			case 1:
+				System.out.print("Nome:");
+				vendedor.setNome(scan.nextLine());
+				break;
+			case 2:
+				System.out.print("CPF:");
+				vendedor.setCpf(scan.nextLine());
+				break;
+			case 3:
+				System.out.print("Data de Nascimento:");
+				try {
+					vendedor.setDataNascimento(this.convertData(scan.nextLine().trim()));
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				break;
+			case 4:
+				System.out.print("Telefone:");
+				vendedor.setTelefone(scan.nextLine());
+				break;
+			case 5:
+				scan.nextLine();
+				System.out.print("Nome:");
+				vendedor.setNome(scan.nextLine());
+				System.out.print("CPF:");
+				vendedor.setCpf(scan.nextLine());
+				System.out.print("Telefone:");
+				vendedor.setTelefone(scan.nextLine());
+				System.out.print("Data de nascimento:");
+				String dataNascimento = scan.nextLine().trim();
+				try {
+					vendedor.setDataNascimento(this.convertData(dataNascimento));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				break;
+			default:
+				System.out.println("Opção Inválida!");
+				break;
+			}
+			vendedorDAO.update(vendedor);
+		}else {
+			System.out.println("Cliente não encontrado");
+		}		
 	}
 
 	@Override
 	public void excluir() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("/--- EXCLUIR VENDEDOR --- /");
+		System.out.print("Insira o código do Vendedor para excuir: ");
+		scan.nextLine();
+		String codigo = scan.nextLine();
+
+		if(codigo != null && codigo != "")
+			vendedorDAO.delete(Long.parseLong(codigo));
+		else
+			System.out.println("Código inválido!");
 	}
 
 	@Override
 	public void menu() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Insira uma das opções abaixo:");
+		System.out.println(" 1- Inserir vendedor;");
+		System.out.println(" 2- Listar vendedores;");
+		System.out.println(" 3- Alterar vendedor.");
+		System.out.println(" 4- Excluir vendedor.");
+
+		int op = scan.nextInt();
+
+		switch(op) {
+		case 1:
+			this.salvar();
+			break;
+		case 2:
+			for(Vendedor vendedor : vendedorDAO.getList()) {
+				System.out.println(vendedor.toString());
+			}
+			break;
+		case 3:
+			this.editar();
+			break;
+		case 4:
+			this.excluir();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private Date convertData(String dataP) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		LocalDate data = LocalDate.of(
+				Integer.parseInt(dataP.substring(6,10)), 
+				Integer.parseInt(dataP.substring(3,5))	, 
+				Integer.parseInt(dataP.substring(0,2)));
+		return sdf.parse(data.toString());
 	}
 
 }
--- src/main/java/teste/SistemaTeste.java
+++ src/main/java/teste/SistemaTeste.java
@@ -1,5 +1,9 @@
 package teste;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Scanner;
 
 import DAO.ClienteDAO;
@@ -8,6 +12,7 @@ import DAO.ProdutosDAO;
 import DAO.VendedorDAO;
 import business.ClienteBusiness;
 import business.ProdutoBusiness;
+import business.VendedorBusiness;
 import entities.Cliente;
 import entities.Pedidos;
 import entities.Produtos;
@@ -18,9 +23,11 @@ public class SistemaTeste {
 	public static PedidoDAO pedidoDAO = new PedidoDAO();
 	public static ClienteBusiness clienteBusiness = new ClienteBusiness();
 	public static ProdutoBusiness produtoBusiness = new ProdutoBusiness();
+	public static VendedorBusiness vendedorBusiness = new VendedorBusiness();
 	public static Scanner scan  = new Scanner(System.in);
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws ParseException {
+
 		System.out.println("Iniciando...");
 		System.out.println("/--------- SISTEMA DE VENDAS DE PRODUTOS------/");
 		System.out.println("1- Vendas");
@@ -29,7 +36,7 @@ public class SistemaTeste {
 		System.out.println("4- Vendedores");
 		System.out.println("Qual módulo deseja usar?");
 		int op = scan.nextInt();
-		
+
 		switch(op) {
 		case 1: break;
 		case 2: 
@@ -38,7 +45,9 @@ public class SistemaTeste {
 		case 3:
 			clienteBusiness.menu();
 			break;
-		case 4: break;
+		case 4: 
+			vendedorBusiness.menu();
+			break;
 		default:
 			System.out.println("Opção Inválida");
 			break;
