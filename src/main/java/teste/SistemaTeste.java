07237a42091df1186516f3dd3acfba7fb56abfd6
LaBatata101 <labatata101@linuxmail.org>
2021-03-30 21:06:41 -0300

    exit program when user press 0 and add method to close all connections and finish program execution

--- src/main/java/teste/SistemaTeste.java
+++ src/main/java/teste/SistemaTeste.java
@@ -7,47 +7,63 @@ import business.PedidoBusiness;
 import business.ProdutoBusiness;
 import business.VendedorBusiness;
 
-import java.text.ParseException;
 import java.util.Scanner;
 
 public class SistemaTeste {
-	public static VendedorDAO vendedorDAO = new VendedorDAO();
-	public static PedidoDAO pedidoDAO = new PedidoDAO();
 	public static ClienteBusiness clienteBusiness = new ClienteBusiness();
 	public static ProdutoBusiness produtoBusiness = new ProdutoBusiness();
 	public static VendedorBusiness vendedorBusiness = new VendedorBusiness();
 	public static PedidoBusiness pedidoBusiness = new PedidoBusiness();
 	public static Scanner scan  = new Scanner(System.in);
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		System.out.println("Iniciando...");
-		System.out.println("/--------- SISTEMA DE VENDAS DE PRODUTOS------/");
-		System.out.println("1- Vendas");
-		System.out.println("2- Produtos");
-		System.out.println("3- Clientes");
-		System.out.println("4- Vendedores");
-		System.out.println("5- Pedidos");
-		System.out.println("Digite o módulo que deseja acessar: ");
-		int op = scan.nextInt();
+		int op;
+		while (true) {
 
-		switch(op) {
-		case 1: break;
-		case 2: 
-			produtoBusiness.menu();
-			break;
-		case 3:
-			clienteBusiness.menu();
-			break;
-		case 4: 
-			vendedorBusiness.menu();
-			break;
-		case 5:
-			pedidoBusiness.menu();
-			break;
-		default:
-			System.out.println("Opção Inválida");
-			break;
+			System.out.println("/--------- SISTEMA DE VENDAS DE PRODUTOS------/");
+			System.out.println("1- Vendas");
+			System.out.println("2- Produtos");
+			System.out.println("3- Clientes");
+			System.out.println("4- Vendedores");
+			System.out.println("5- Pedidos");
+			System.out.println("0- Sair");
+			System.out.println("Digite o módulo que deseja acessar: ");
+			op = scan.nextInt();
+
+			switch(op) {
+				case 0:
+					closeConnectionAndExit();
+					break;
+				case 1:
+					System.out.println("Não implementado YET!!!!!!!!");
+					break;
+				case 2:
+					produtoBusiness.menu();
+					break;
+				case 3:
+					clienteBusiness.menu();
+					break;
+				case 4:
+					vendedorBusiness.menu();
+					break;
+				case 5:
+					pedidoBusiness.menu();
+					break;
+				default:
+					System.out.println("Opção Inválida");
+					break;
+			}
 		}
-	}	
+	}
+
+	public static void closeConnectionAndExit() {
+		produtoBusiness.closeDAOConnection();
+		vendedorBusiness.closeDAOConnection();
+		clienteBusiness.closeDAOConnection();
+		pedidoBusiness.closeDAOConnection();
+
+		System.exit(0);
+	}
 }
